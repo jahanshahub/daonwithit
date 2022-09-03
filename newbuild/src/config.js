96e38e755207292416937a4934141da334e16178
Mohsen Jahanshahiu <mohsenarthur@gmail.com>
2022-09-03 12:15:45 -0700

    notes

---
+++ newbuild/src/config.js
@@ -0,0 +1,9 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+
+export const NFTCONTRACT = "0x597e400611DC566A16552FE7bB353500733cDe5c";
+export const STAKINGCONTRACT = "0x2c2c58eF8ee835D92986Dc46c626d479BB4e8239";
+export const etherscanapi = "https://api-goerli.etherscan.io/";
+export const moralisapi = "https://deep-index.moralis.io/api/v2";
+export const nftpng = "https://ipfs.io/ipfs/QmNVNxwDue3Bb9dbaesEj5zGJESir2GYJ4Jm2vkJbZaHPZ/";
+export const Web3Alc = createAlchemyWeb3("https://eth-goerli.g.alchemy.com/v2/vY76_mMjGO0EPfRIs2q_S4zsVOn1w-vF");
+
